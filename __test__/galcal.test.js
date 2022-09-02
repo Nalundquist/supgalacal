30d6833f8ff736b9126d8305e4d1dcbf5e594bf9
Noah Lundquist <nalundquist@gmail.com>
2022-09-02 10:15:50 -0700

    write full lifeExp test one; fail

--- __test__/galcal.test.js
+++ __test__/galcal.test.js
@@ -28,4 +28,14 @@ describe ('GalAge', () => {
 	test('galCal method should take this.earthAge and convert it to Jupiter Years (rounded to the nearest integer)', () => {
 		expect(newAge.jupiAge).toEqual(6)
 	})
+
+	test('lifeExp method should calculate average earth life expectancy modified by piousness; apply that to previous planet calculations', () => {
+		newAge.piety = "god-fearing" 
+		newAge.lifeExp();
+		expect(newAge.earthExp).toEqual(91)
+		expect(newAge.mercExp).toEqual(379)
+		expect(newAge.venusExp).toEqual(147)
+		expect(newAge.marsExp).toEqual(48)
+		expect(newAge.jupiExp).toEqual(8)
+	})
 })
