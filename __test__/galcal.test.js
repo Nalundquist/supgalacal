05590be4f829df5e47133cf6bd662a84bb5c1c6a
Noah Lundquist <nalundquist@gmail.com>
2022-09-02 09:40:24 -0700

    add third GalAge test; fail

--- __test__/galcal.test.js
+++ __test__/galcal.test.js
@@ -6,6 +6,7 @@ describe ('GalAge', () => {
 
 	beforeEach(() => {
 		newAge = new GalAge(72);
+		newAge.galCal();
 	});
 
 	test('Should be a constructor capable of holding an earthAge key:value pair', () => {
@@ -14,5 +15,9 @@ describe ('GalAge', () => {
 
 	test('galCal method should take this.earthAge and convert it to Mercury Years (rounded to the nearest integer)', () => {
 		expect(newAge.mercAge).toEqual(300);
+	})
+
+	test('galCal method should take this.earthAge and convert it to Venus Years (rounded to the nearest integer)', () => {
+		expect(newAge.VenusAge).toEqual(116)
 	})
 })
--- src/galcal.js
+++ src/galcal.js
@@ -2,4 +2,8 @@ export default class GalAge {
 	constructor(age){
 		this.earthAge = age;
 	}
+
+	galCal() {
+		this.mercAge = Math.round(this.earthAge / .24);
+	}
 } 
